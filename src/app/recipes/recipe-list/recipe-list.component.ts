0f99eb01321b857f761ff6b5b1c307f6dfcbf93c
jna2158 <jna2158@gmail.com>
2022-07-29 09:59:00 +0900

    ~shoppint list 추가

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,12 +7,18 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  @Output() recipeWasSelected = new EventEmitter<Recipe>(); // 부모 컴포넌트인 recipes.component.html로 데이터를 방출한다.
 
   recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
+      'https://www.simplyrecipes.com/thmb/trN7dD0wpgfPs8ArTWySkrjaMdQ=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Brushetta-With-Tomato-And-Basil-Lead-Shot-6b-e52628e5a6f14fc3af66de40e8f5b035.jpg'
+    ),
+
+    new Recipe(
+      'A TTTTTTTest Recipe',
+      'This is simply a ttttttttttest',
       'https://www.simplyrecipes.com/thmb/trN7dD0wpgfPs8ArTWySkrjaMdQ=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Brushetta-With-Tomato-And-Basil-Lead-Shot-6b-e52628e5a6f14fc3af66de40e8f5b035.jpg'
     ),
   ];
@@ -21,5 +27,7 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  onSelected(recipe: Recipe) {}
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
