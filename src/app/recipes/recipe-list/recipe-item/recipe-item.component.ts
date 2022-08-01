060acf9f08f694aa8bf4cb6d62c4f6721b5093e3
jna2158 <jna215843@gmail.com>
2022-08-01 16:51:38 +0900

    navigation bar 라우팅

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,12 +8,7 @@ import { RecipeService } from '../../recipe.service';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe; // @Input을 붙여 외부 컴포넌트로부터 입력받는 데이터임을 표시한다.
-
-  constructor(private recipeService: RecipeService) {}
+  @Input() index: number;
 
   ngOnInit(): void {}
-
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
 }
